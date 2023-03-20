6a66c77074d14b75632014bc786956c7f436b19d
Ana Cervantes <85836732+Aanacrr-1@users.noreply.github.com>
2023-03-20 12:12:52 -0500

    Hours_worked

---
+++ main (2).py
@@ -0,0 +1,8 @@
+
+hours_worked = input("Enter hours worked: ")
+hourly_pay_rate = float(input("Enter hourly pay rate: "))
+
+#Calulate - hours worked multiplied by pay rate
+pay = int(hours_worked) * hourly_pay_rate
+
+print("Gross Pay:", pay)
